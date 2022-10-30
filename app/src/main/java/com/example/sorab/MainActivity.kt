13abfa620abb2cb90ac2d812f9a7ea31f00761bb
jinhaaya <jhkmo51@gmail.com>
2022-10-30 23:53:27 +0900

    theme

--- app/src/main/java/com/example/sorab/MainActivity.kt
+++ app/src/main/java/com/example/sorab/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.sorab
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         setTheme(R.style.Theme_Sorab)
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = this.resources.getColor(R.color.white)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
