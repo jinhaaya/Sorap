ef34e1e58aa119668100331bbf49567cdb2fa4f0
jinhaaya <jhkmo51@gmail.com>
2022-11-01 05:58:16 +0900

    ss

--- app/src/main/java/com/example/sorap/MainActivity.kt
+++ app/src/main/java/com/example/sorap/MainActivity.kt
@@ -3,25 +3,29 @@ package com.example.sorap
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     @SuppressLint("UseSupportActionBar")
     override fun onCreate(savedInstanceState: Bundle?) {
-
         // Loading 종료 후 메인화면으로 변경
         setTheme(R.style.Theme_Sorab)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // ActionBar 설정
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)       // main_menu 메뉴를 toolbar 메뉴 버튼으로 설정
+        // main_menu 메뉴를 toolbar 메뉴 버튼으로 설정
+        menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
 }
