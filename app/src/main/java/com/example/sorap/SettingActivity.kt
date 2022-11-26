07bd75945ed65d7e45c551462479a00e48917d9b
jinhaaya <jhkmo51@gmail.com>
2022-11-26 22:46:04 +0900

    setting_preferences

--- app/src/main/java/com/example/sorap/SettingActivity.kt
+++ app/src/main/java/com/example/sorap/SettingActivity.kt
@@ -3,6 +3,7 @@ package com.example.sorap
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
 import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity(){
@@ -10,13 +11,23 @@ class SettingActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
-
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
         // ActionBar 설정
         setSupportActionBar(toolbar_setting)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.setting_preferences, rootKey)
+        }
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.getItemId()) {
